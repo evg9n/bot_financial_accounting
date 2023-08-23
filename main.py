fa5e8407ed8ec0274a61ea8f3e064b74205c6969
Емельянов Евгений <e.evgeny999@gmail.com>
2023-08-23 23:30:31 +0300

    add ignore logs

--- main.py
+++ main.py
@@ -2,9 +2,52 @@ import handlers
 from loader import bot
 from telebot.custom_filters import StateFilter
 from telebot.types import BotCommand
+from os.path import abspath, join
+from logging import config, getLogger, DEBUG, INFO
 
-from os import listdir, path
 
+FORMAT = "%(levelname)-4s %(name)s [%(asctime)s] %(message)s [line: %(lineno)d]"
+datefmt = '%d.%m.%y %H:%M:%S'
+
+level = DEBUG
+
+log_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standart': {
+            'format': FORMAT,
+            'datefmt': datefmt
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': level,
+            'formatter': 'standart',
+            'stream': 'ext://sys.stdout'
+        },
+        'file_handler': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': level,
+            'filename': abspath(join('logs', 'log.log')),
+            'encoding': 'utf-8',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 500,
+            'formatter': 'standart'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file_handler', 'console'],
+            'level': level
+        },
+    }
+}
+
+config.dictConfig(log_config)
+log = getLogger()
 
 # DEFAULT_COMMANDS = (
 #     ('start', "Запустить бота"),
