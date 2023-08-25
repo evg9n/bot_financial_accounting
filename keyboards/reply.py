a947b8b866b14166a26937122233c22fd0f6ff02
Evgen <e.evgeny999@gmail.com>
2023-08-25 15:13:28 +0300

    добавлена клавиатура со списком

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,11 +1,24 @@
 from telebot.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from work_database.get import get_names_finance
 
 
 main_menu_buttons = ('Мои финансы', )
+BUTTONS_ADD_FINANCE = 'Создать'
 
 
-def main_menu():
+def main_menu() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     buttons = [KeyboardButton(text=button) for button in main_menu_buttons]
+    markup.add(*buttons)
+    return markup
+
+
+def list_finance(user_id: int) -> ReplyKeyboardMarkup:
+    markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    buttons = list()
+    lst = get_names_finance(user_id=user_id)
+    for name in lst:
+        buttons.append(KeyboardButton(text=name[0]))
+    buttons.append(KeyboardButton(text=BUTTONS_ADD_FINANCE))
     markup.add(*buttons)
     return markup
