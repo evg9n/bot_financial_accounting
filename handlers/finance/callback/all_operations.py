985d901be42d13f3adfb51a6c73e09eb09609df8
Evgen <e.evgeny999@gmail.com>
2023-09-04 17:40:53 +0300

    Добавлены календари

--- handlers/finance/callback/all_operations.py
+++ handlers/finance/callback/all_operations.py
@@ -1,41 +1,111 @@
 from loader import bot
-from keyboards.inline.all_operations import BUTTONS_SELECT_PERIOD
+from keyboards.inline.all_operations import BUTTONS_SELECT_PERIOD, select_period
 from re import sub
 from telebot.types import CallbackQuery
 from datetime import date, timedelta
 
+from states.finance import NAME_TABLE_FINANCE
+from utils.plug import plug
+from work_database.get import get_state, get_state_date
+from work_database.set import set_state_date
+from utils.calendar import Calendar, LSTEP
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith('select_period_operation_'))
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith('select_period_operation_') and
+                                               get_state(user_id=call.from_user.id) in (NAME_TABLE_FINANCE, )))
 async def select_period_operation(call: CallbackQuery):
+    """Обработка начала периода"""
     text = sub(pattern='select_period_operation_', repl='', string=call.data)
+    user_id = call.from_user.id
+    message_id = call.message.message_id
 
     if text == BUTTONS_SELECT_PERIOD[0]:
         today = date.today()
+        set_state_date(user_id=user_id, date=today)
+        set_state_date(user_id=user_id, date=today, column_date2=True)
+        await plug(user_id=user_id, message_id=message_id, edit=True)
 
     elif text == BUTTONS_SELECT_PERIOD[1]:
         yesterday = date.today() - timedelta(days=1)
+        set_state_date(user_id=user_id, date=yesterday)
+        await bot.edit_message_text(chat_id=user_id, text='Выбери окончание периода:', message_id=message_id,
+                              reply_markup=select_period(start=False, button_manually=False, button_month=False))
 
     elif text == BUTTONS_SELECT_PERIOD[2]:
         today = date.today()
         start_month = today - timedelta(days=today.day)
+        set_state_date(user_id=user_id, date=start_month)
+        set_state_date(user_id=user_id, date=today, column_date2=True)
+        await plug(user_id=user_id, message_id=message_id, edit=True)
 
     elif text == BUTTONS_SELECT_PERIOD[3]:
-        ...
+        calendar_inline, step = Calendar(calendar_id=1).build()
+        await bot.edit_message_text(chat_id=user_id, message_id=message_id, text=f'Выбери {LSTEP[step]} начала:',
+                                    reply_markup=calendar_inline)
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith('select_period_2_operation_'))
+@bot.callback_query_handler(func=lambda call: (call.data.startswith('cbcal_1') and
+                                               get_state(user_id=call.from_user.id) in (NAME_TABLE_FINANCE, )))
+async def calendar_1(call: CallbackQuery):
+    """Обработка календаря ОТ"""
+    result, key, step = Calendar(calendar_id=1).process(call_data=call.data)
+    user_id = call.from_user.id
+    message_id = call.message.message_id
+
+    if not result and key:
+        await bot.edit_message_text(text=f"Выбери {LSTEP[step]} начала:",
+                                    chat_id=user_id,
+                                    message_id=message_id,
+                                    reply_markup=key)
+    elif result:
+        set_state_date(user_id=user_id, date=result)
+        await bot.edit_message_text(chat_id=user_id, message_id=message_id,
+                                    text="Выбери окончание периода:",
+                                    reply_markup=select_period(start=False, button_month=False))
+
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith('select_period_2_operation_') and
+                                               get_state(user_id=call.from_user.id) in (NAME_TABLE_FINANCE, )))
 async def select_period_operation(call: CallbackQuery):
+    """Выбор окончание периода"""
     text = sub(pattern='select_period_2_operation_', repl='', string=call.data)
+    user_id = call.from_user.id
+    message_id = call.message.message_id
 
     if text == BUTTONS_SELECT_PERIOD[0]:
         today = date.today()
+        set_state_date(user_id=user_id, column_date2=True, date=today)
+        await plug(user_id=user_id, message_id=message_id, edit=True)
 
     elif text == BUTTONS_SELECT_PERIOD[1]:
         yesterday = date.today() - timedelta(days=1)
+        set_state_date(user_id=user_id, column_date2=True, date=yesterday)
+        await plug(user_id=user_id, message_id=message_id, edit=True)
 
-    elif text == BUTTONS_SELECT_PERIOD[2]:
-        today = date.today()
-        start_month = today - timedelta(days=today.day)
+    # elif text == BUTTONS_SELECT_PERIOD[2]:
+    #     today = date.today()
+    #     start_month = today - timedelta(days=today.day)
 
     elif text == BUTTONS_SELECT_PERIOD[3]:
-        ...
+        calendar_inline, step = Calendar(calendar_id=2).build()
+        await bot.edit_message_text(chat_id=user_id, message_id=message_id, text=f'Выбери {LSTEP[step]} окончание:',
+                                    reply_markup=calendar_inline)
+
+
+@bot.callback_query_handler(func=lambda call: (call.data.startswith('cbcal_2') and
+                                               get_state(user_id=call.from_user.id) in (NAME_TABLE_FINANCE, )))
+async def calendar_2(call: CallbackQuery):
+    """Обработка календаря ДО"""
+    user_id = call.from_user.id
+    message_id = call.message.message_id
+    min_date = get_state_date(user_id=user_id)
+    result, key, step = Calendar(calendar_id=2, min_date=min_date).process(call_data=call.data)
+
+    if not result and key:
+        await bot.edit_message_text(text=f"Выбери {LSTEP[step]} окончание:",
+                                    chat_id=user_id,
+                                    message_id=message_id,
+                                    reply_markup=key)
+    elif result:
+        set_state_date(user_id=user_id, date=result, column_date2=True)
+        await plug(user_id=user_id, message_id=message_id, edit=True)
--- keyboards/inline/all_operations.py
+++ keyboards/inline/all_operations.py
@@ -4,15 +4,35 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 BUTTONS_SELECT_PERIOD = ("За сегодня", "Вчера", "За текущий месяц", "Указать вручную", )
 
 
-def select_period(start: bool = True) -> InlineKeyboardMarkup:
+def select_period(start: bool = True, button_today: bool = True, button_yesterday: bool = True,
+                  button_month: bool = True, button_manually: bool = True) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup(row_width=1)
 
     callback_data = 'select_period_2_operation_'
     if start:
         callback_data = 'select_period_operation_'
 
-    buttons = [
-        InlineKeyboardButton(text=text, callback_data=f'{callback_data}{text}') for text in BUTTONS_SELECT_PERIOD
-    ]
+    buttons = list()
+
+    if button_today:
+        b = InlineKeyboardButton(text=BUTTONS_SELECT_PERIOD[0],
+                                 callback_data=f'{callback_data}{BUTTONS_SELECT_PERIOD[0]}')
+        buttons.append(b)
+
+    if button_yesterday:
+        b = InlineKeyboardButton(text=BUTTONS_SELECT_PERIOD[1],
+                                 callback_data=f'{callback_data}{BUTTONS_SELECT_PERIOD[1]}')
+        buttons.append(b)
+
+    if button_month:
+        b = InlineKeyboardButton(text=BUTTONS_SELECT_PERIOD[2],
+                                 callback_data=f'{callback_data}{BUTTONS_SELECT_PERIOD[2]}')
+        buttons.append(b)
+
+    if button_manually:
+        b = InlineKeyboardButton(text=BUTTONS_SELECT_PERIOD[3],
+                                 callback_data=f'{callback_data}{BUTTONS_SELECT_PERIOD[3]}')
+        buttons.append(b)
+
     markup.add(*buttons)
     return markup
